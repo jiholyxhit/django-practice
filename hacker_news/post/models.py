c8b38f8414a246fd5d556c93e2966af513d05b4d
jihowoo <jihobbit8677@gmail.om>
2025-07-01 18:33:42 +0900

    add post models and migrate

--- hacker_news/post/models.py
+++ hacker_news/post/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=128)
+    body = models.CharField(max_length=1024)
+    author_name = models.CharField(max_length=32)
+    points = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
