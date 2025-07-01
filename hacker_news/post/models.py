9f60143511ddd2dc3a77d2eaee4d54ee49307012
jihowoo <jihobbit8677@gmail.om>
2025-07-01 20:12:25 +0900

    modify DB objects and override model __str__ output format

--- hacker_news/post/models.py
+++ hacker_news/post/models.py
@@ -8,3 +8,6 @@ class Post(models.Model):
     author_name = models.CharField(max_length=32)
     points = models.PositiveIntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.pk}, {self.title}"
