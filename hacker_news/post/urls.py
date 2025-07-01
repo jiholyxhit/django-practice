ea78f769c11528134bcd8f300db7afb001367695
jihowoo <jihobbit8677@gmail.om>
2025-07-01 21:23:00 +0900

    practice get posts

--- hacker_news/post/urls.py
+++ hacker_news/post/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from post import views
 
 urlpatterns = [
-    path("index/", views.index_view, name="post_index")
+    # path("index/", views.index_view, name="post_index")
+    path("", views.posts_view, name="posts")
 ]
--- hacker_news/post/views.py
+++ hacker_news/post/views.py
@@ -1,6 +1,13 @@
 from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
 
-def index_view(request):
-    return HttpResponse("Hello, there!")
+# def index_view(request):
+#     return HttpResponse("Hello, there!")
+
+def posts_view(request):
+    posts = Post.objects.all()
+    result = ", ".join([p.title for p in posts])
+    return HttpResponse(result)
+    
