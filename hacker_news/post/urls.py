171ed9c7e3b17e940bc6452075af3b2f7fd1bfc8
jihowoo <jihobbit8677@gmail.om>
2025-07-03 16:05:59 +0900

    style: apply static css file to MVT model - using BootStrap CDN

--- hacker_news/post/urls.py
+++ hacker_news/post/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 from post import views
 
 urlpatterns = [
@@ -6,5 +8,4 @@ urlpatterns = [
     path("", views.posts_view, name="posts"),
     path("<int:post_id>", views.post_view, name="post"),
     path("<int:post_id>/like/", views.post_like_view, name="post_like")
-
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
