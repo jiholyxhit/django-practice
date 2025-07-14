1aea70b810b5e36043e8d7733ee58f41d0fc7d61
jihowoo <jihobbit8677@gmail.om>
2025-07-14 17:51:56 +0900

    feature: add kakao social login - redirect to Kako server, Kakao Developer auth_code App REST_API KEY & callback URL, HTTP python requests module

--- auth_user/auth_user/user/urls.py
+++ auth_user/auth_user/user/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path("", views.HomeView.as_view(), name="home"),
     path("users/", include("django.contrib.auth.urls")),
     path("users/sing-up/", views.SignUpView.as_view(), name="sign_up"),
-       
+    path("useres/social/kakao/login/", views.KakaoSocialLoginView.as_view(), name="kakao_social_login")
 ]
 
--- auth_user/auth_user/user/views.py
+++ auth_user/auth_user/user/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.views import View
 from django.contrib.auth import login
 from user.forms import CustomUserCreationForm
+from django.config import settings
 
 # Create your views here.
 
@@ -23,5 +24,14 @@ class SignUpView(View):
             user = form.save()
             login(request, user)
             return redirect("home")
+
+class KakaoSocialLoginView(View):
+    def get(self, request):
+        return redirect(
+            f"https://kauth.kakao.com/oauth/authorize"
+            f"?client_id={settings.KAKAO_REST_API_KEY}"
+            f"&redirect_uri={settings.KAKAO_CALLBACK_URL}"
+            f"&response_type=code"
+        )
     
 
