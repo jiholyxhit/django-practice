ce762402781fb32e97f69b0df6ae9125086d804d
jihowoo <jihobbit8677@gmail.om>
2025-07-15 18:22:46 +0900

    feat: add home that shows all the shortened urls - model field unique property, model class Meta overriding, django templae language forloop object/{%empty%}, HTML table_thead/tbody/tr/td

--- short_url/short_url/shortener/models.py
+++ short_url/short_url/shortener/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+
+class ShortURL(models.Model):
+    code = models.CharField(max_length=8, unique=True)
+    original_url = models.URLField(max_length=200)
+    access_count = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add = True)
+
+    class Meta:
+        app_label = "shortener"
+        db_table = "short_url"
--- short_url/short_url/shortener/views.py
+++ short_url/short_url/shortener/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import View
+
+from shortener.models import ShortURL
 
 # Create your views here.
+
+class HomeView(View):
+    def get(self, request):
+        short_urls = ShortURL.objects.all()
+        return render(request, "home.html", {"short_urls": short_urls})
